890ccf8b1ad4090fbde3496e5af2bd445ffedb96
Matt Innes <matthew.f.innes@gmail.com>
2021-07-09 22:28:02 -0400

    Repository advanced search clarity
    - Makes 'thematic area', 'type', and 'region' select forms reflect current parameters

--- apps/qubit/modules/repository/templates/_advancedFilters.php
+++ apps/qubit/modules/repository/templates/_advancedFilters.php
@@ -10,9 +10,11 @@
       <div class="span4">
         <label for="thematicAreas"><?php echo __('Thematic area:'); ?></label>
         <select name="thematicAreas" id="thematicAreas">
-          <option selected="selected"></option>
+          <option></option>
           <?php foreach ($thematicAreas as $r) { ?>
-            <option value="<?php echo $r->getId(); ?>">
+            <option value="<?php echo $r->getId(); ?>"<?php if ($r->getId() == $sf_request->getParameter('thematicAreas')) { ?>
+              <?php echo selected; ?>
+            <?php } ?>>
               <?php echo get_search_i18n($r->getData(), 'name', ['cultureFallback' => true]); ?>
             </option>
           <?php } ?>
@@ -22,9 +24,11 @@
       <div class="span4">
         <label for="types"><?php echo __('Archive type:'); ?></label>
         <select name="types" id="types">
-          <option selected="selected"></option>
+          <option></option>
           <?php foreach ($repositoryTypes as $r) { ?>
-            <option value="<?php echo $r->getId(); ?>">
+            <option value="<?php echo $r->getId(); ?>"<?php if ($r->getId() == $sf_request->getParameter('types')) { ?>
+              <?php echo selected; ?>
+            <?php } ?>>
               <?php echo get_search_i18n($r->getData(), 'name', ['cultureFallback' => true]); ?>
             </option>
           <?php } ?>
@@ -34,14 +38,18 @@
       <div class="span4">
         <label for="regions"><?php echo __('Region:'); ?></label>
         <select name="regions" id="regions">
-          <option selected="selected"></option>
+          <option></option>
           <?php $regions = []; ?>
           <?php foreach ($repositories as $r) { ?>
             <?php $region = get_search_i18n($r->getData(), 'region', ['allowEmpty' => true, 'culture' => $sf_user->getCulture(), 'cultureFallback' => false]); ?>
 
             <?php if ($region && !in_array($region, $regions)) { ?>
               <?php $regions[] = $region; ?>
-              <option value="<?php echo $region; ?>"><?php echo $region; ?></option>
+              <option value="<?php echo $region; ?>"<?php if ($region == $sf_request->getParameter('regions')) { ?>
+                <?php echo selected; ?>
+              <?php } ?>>             
+                <?php echo $region; ?>
+              </option>
             <?php } ?>
           <?php } ?>
         </select>
